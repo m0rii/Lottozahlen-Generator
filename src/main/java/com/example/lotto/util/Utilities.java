a5dc77f997cc686027de1d6e319ab6606ff0f365
Morteza Nabhanklhoveiz <mon@hbt.de>
2021-11-18 13:21:55 +0100

    add junit test for Utilities class

--- src/main/java/com/example/lotto/util/Utilities.java
+++ src/main/java/com/example/lotto/util/Utilities.java
@@ -1,13 +1,12 @@
 package com.example.lotto.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Utilities dient als Helper Klasse
@@ -15,37 +14,39 @@ import java.util.stream.Collectors;
  * @author Morteza Nabhan
  */
 public class Utilities {
-    private static final Logger logger = LoggerFactory.getLogger(Utilities.class.getName());
-    private static final Scanner scanner = new Scanner(System.in);
+  private static final Logger logger = LoggerFactory.getLogger(Utilities.class.getName());
+  private static final Scanner scanner = new Scanner(System.in);
 
-    /**
-     * konventiert ein String zu einem List von Integer
-     *
-     * @param input eine String von mit Komma geterenden zahlen "5,12,33,45"
-     * @return eine List von Integer
-     * @throws NumberFormatException wenn String zu Integer nicht konventiert kann
-     */
-    public static List<Integer> stringToIntList(String input) {
-        try {
-            List<String> list = Arrays.asList(input.split(","));
-            return list.stream().map(Integer::parseInt).collect(Collectors.toList());
-        } catch (NumberFormatException e) {
-            List<Integer> emptyList = new ArrayList<>();
-            logger.error("parsing error : {} ", e.getMessage());
-            return emptyList;
-        }
+  /**
+   * konventiert ein String zu einem List von Integer
+   *
+   * @param input eine String von mit Komma geterenden zahlen "5,12,33,45"
+   * @return eine List von Integer
+   * @throws NumberFormatException wenn String zu Integer nicht konventiert kann
+   */
+  public static List<Integer> stringToIntList(String input) {
 
+    try {
+      List<String> list = Arrays.asList(input.split(","));
+      return list.stream().map(Integer::parseInt).collect(Collectors.toList());
+    } catch (NumberFormatException | NullPointerException e) {
+      logger.error("parsing error : {} ", e.getClass() + " : " + e.getMessage());
+      return new ArrayList<>();
     }
+  }
 
-    /**
-     * konventiert ein List von Integer zu einem mit Kommagetrennten String
-     *
-     * @param list ein List von Integer
-     * @return ein String
-     */
-    public static String listIntToString(List<Integer> list) {
-        return list.stream().map(String::valueOf).collect(Collectors.joining(","));
+  /**
+   * konventiert ein List von Integer zu einem mit Kommagetrennten String
+   *
+   * @param list ein List von Integer
+   * @return ein String
+   */
+  public static String listIntToString(List<Integer> list) {
+    try {
+      return list.stream().map(String::valueOf).collect(Collectors.joining(","));
+    } catch (NullPointerException e) {
+      logger.error("list in listIntToString is null : {} ", e.getClass() + " : " + e.getMessage());
+      return "";
     }
-
-
+  }
 }
